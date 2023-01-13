06cdcf6563eb7209d46fd87ff338faf10e142d27
Shailesh <gajbhiye719@gmail.com>
2023-01-13 11:34:22 +0530

    Solution 1 Encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+         RWOnly obj1 = new RWOnly() ;
+         // name has a Private access in 'com.driver.RWOnly''
+        obj1.setName("Cool") ;
+        String name = obj1.getName();
+        System.out.println(name) ;
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name ;
+   // setter
+    void setName(String name){
+        this.name = name ;
+    }
+
+     // getter
+    String getName(){
+        return name ;
+    }
+
 }
